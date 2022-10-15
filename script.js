0df0f9e8f51f96223bdff36eb3ddbd2c8df2fde8
Delia S <delias@MacBook-Air-Delia.local>
2022-10-15 12:35:39 +0300

    okay

---
+++ script.js
@@ -0,0 +1,28 @@
+function myFirstApp(name, age) {
+alert(`hello, my name is ${name} and its my first app`);
+function showSkills() {
+    let skills = ['html', 'css', 'js'];
+console.log('i know ' + skills );
+
+}
+
+showSkills();
+
+showSkills
+function checkAge(age) {
+    alert('whats your age ${name}?');
+    if (age < 18) 
+    alert('too young');
+    else {
+        alert('youre good');
+    }
+}
+function caclPow(a, b) {
+    console.log(a ** b);
+}
+
+caclPow(5, 2);
+
+
+
+myFirstapp('Ivan', 32);
